8f5c71e1bf098efae4344940ea8f2bfffc1cd983
Olivia Buckley <olivia.buckley7898@gmail.com>
2025-02-21 09:42:27 +0000

    create a user context for a hard-coded user

--- src/App.jsx
+++ src/App.jsx
@@ -4,21 +4,24 @@ import { BrowserRouter, Routes, Route } from "react-router";
 import PropertiesList from "./components/PropertiesList";
 import Footer from "./components/Footer";
 import PropertyDetails from "./components/PropertyDetails";
+import UserProvider from "./providers/UserProvider";
 
 function App() {
   return (
     <>
       <BrowserRouter>
-        <Header />
-        <NavBar />
-        <Routes>
-          <Route path="/" element={<PropertiesList />}></Route>
-          <Route
-            path="/properties/:property_id"
-            element={<PropertyDetails />}
-          ></Route>
-        </Routes>
-        <Footer />
+        <UserProvider>
+          <Header />
+          <NavBar />
+          <Routes>
+            <Route path="/" element={<PropertiesList />}></Route>
+            <Route
+              path="/properties/:property_id"
+              element={<PropertyDetails />}
+            ></Route>
+          </Routes>
+          <Footer />
+        </UserProvider>
       </BrowserRouter>
     </>
   );
---
+++ src/providers/UserProvider.jsx
@@ -0,0 +1,20 @@
+import { UserContext } from "../contexts/UserContext";
+
+export default function UserProvider({ children }) {
+  const hardcodedUser = {
+    user_id: 2,
+    first_name: "Bob",
+    surname: "Smith",
+    email: "bob@example.com",
+    phone_number: "+44 7000 222222",
+    role: "guest",
+    avatar: "https://example.com/images/bob.jpg",
+    created_at: "2024-12-13T10:29:41.241Z",
+  };
+
+  return (
+    <UserContext.Provider value={hardcodedUser}>
+      {children}
+    </UserContext.Provider>
+  );
+}
